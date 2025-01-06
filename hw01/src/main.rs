5f9accf62026c26daa709fe3b0375a3d284c54f6
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2025-01-06 18:41:37 +0000

    hw01 init
    problem1 function prototypes

---
+++ hw01/src/main.rs
@@ -0,0 +1,25 @@
+use
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+this becomes unweildy and nasty so instead we make seperate .rs files in /src
+
+pub mod problem1 {
+
+}
+
+pub mod problem2 {
+
+}
+
+pub mod problem3 {
+
+}
+
+pub mod problem4 {
+
+}
+*/
---
+++ hw01/src/problem1.rs
@@ -0,0 +1,16 @@
+// problem one
+
+// pub ~ makes this globally available; when normally it is private to this file
+
+pub fn sum(slice: &[i32]) -> i32 {
+    // todo
+}
+
+pub fn dedup(vs: &Vec<i32>) -> Vec<i32> {
+    // todo
+}
+
+pub fn filter(vs: &Vec<i32>, pred: &Fn(i32) -> bool) -> Vec<i32> {
+    // todo
+}
+
